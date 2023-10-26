a534327f1ba56662c8dbd57b7cfbbcca6c9c7c39
imRahul05 <rahulkumar20000516@gmail.com>
2023-10-26 11:13:17 +0530

    firstcommit

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
     const form = document.getElementById("feedback-form");
-    const successMessage = document.getElementById("success-message");
 
     form.addEventListener("submit", function (e) {
         e.preventDefault();
@@ -11,22 +10,17 @@ document.addEventListener("DOMContentLoaded", function () {
         const satisfaction = document.getElementById("satisfaction").value;
         const feedback = document.getElementById("feedback").value;
 
-        // You can handle the form data here, e.g., send it to a server
+        
         console.log("Name: " + name);
         console.log("Phone: " + phone);
         console.log("Email: " + email);
         console.log("Overall Satisfaction: " + satisfaction);
         console.log("Specific Feedback: " + feedback);
 
-        // Show the success message
-        successMessage.style.display = "block";
+        
+        alert("Successfully Submitted!");
 
-        // Clear the form fields
+        
         form.reset();
-
-        // Hide the success message after a few seconds (optional)
-        setTimeout(function () {
-            successMessage.style.display = "none";
-        }, 3000); // Hides the message after 3 seconds (adjust as needed)
     });
 });
